e0e18854189ed5301263c70afb5ac2751152b9d7
Vitor Nunes <vitor_sa2@hotmail.com>
2018-07-05 19:52:01 -0300

    Update memfile.s
    Adequação inicial ao problema da dupla, preparando PG

--- memfile.s
+++ memfile.s
@@ -5,18 +5,17 @@
 // TST, LSL, CMN, ADC
 // If successful, it should write the value 7 to address 100
 
-MAIN	SUB R0, R15, R15 		; R0 = 0				
-	ADD R2, R0, #5      	; R2 = 5             
-	ADD R3, R0, #12    	; R3 = 12            
-	SUB R7, R3, #9    	; R7 = 3             
-	ORR R4, R7, R2    	; R4 = 3 OR 5 = 7              	  		
-        AND R5, R3, R4    	; R5 = 12 AND 7 = 4            	
-	ADD R5, R5, R4    	; R5 = 4 + 7 = 11              			
-        SUBS R8, R5, R7    	; R8 <= 11 - 3 = 8, set Flags   	  		
-        BEQ END        		; shouldn't be taken            	  		
-        SUBS R8, R3, R4    	; R8 = 12 - 7  = 5             			
-        BGE AROUND       	; should be taken               
-	ADD R5, R0, #0     	; should be skipped             	
+MAIN					
+	XOR R5, R5      	; zera registrador             
+	XOR R7, R7    		;             
+	XOR R0, R0	    	;              
+	XOR R4, R4	    	;               	  		
+        AND R5, R5, #2    	; R5 recebe NUmero base da PG (2)           	
+	ADD R7, R7, R5    	; Valor da PG deve ser adicionado no registrador da dupla              			
+        ADD R0, R0, #200
+	ADD R0, R0, #200
+	ADD R0, R0, #200
+	ADD R0, R0, #100    ;X=7 (posicao base da memoria = X*100)
 AROUND   
 	SUBS R8, R7, R2   	; R8 = 3 - 5 = -2, set Flags   	         	
         ADDLT R7, R5, #1  	; R7 = 11 + 1 = 12				          	
